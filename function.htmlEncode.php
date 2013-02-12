a4fe80e6d68b94bc16c8800af55871d499a151d3
Sean Macdonald <stuff@crazyhorsecoding.com>
2013-02-12 15:00:41 -0500

    changed dirs

--- function.htmlEncode.php
+++
@@ -1,5 +0,0 @@
-<?php
-function htmlEncode($var) {
-	return htmlentities($var, ENT_QUOTES, 'UTF-8') ;
-}
-?>
--- function.isAssoc.php
+++
@@ -1,9 +0,0 @@
-<?php
-	function isAssoc($array) {
-		if (!is_array($array)) {
-			return false;
-		}
-    	$array = array_keys($array);
-    	return ($array !== array_keys($array));
-	}
-?>
